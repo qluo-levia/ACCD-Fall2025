eda8fa2ebc54104b994e616e73f4b9b7a2961e90
qluo-levia <qluo@inside.artcenter.edu>
2025-10-23 23:27:38 -0700

    .

--- week 05/arcs.js
+++ week 05/arcs.js
@@ -1,22 +1,24 @@
-let numArcs = 10
-let initRadius
+let numRings = 10
+let radius
 let strWeight = 20
 function setup(){
     createCanvas(800,800)
     colorMode(HSB, TWO_PI,1,1)
-    initRadius = width *0.1
 
-    noFill()
+    radius = width *0.1
+    strokeCap(circle)
 }
 
 function draw(){
     background(0)
+    noFill()
     strokeWeight(strWeight)
-    for (let i = 0 ; i < numArcs; i++){
-        stroke(0,1,1)
+    for (let i = 0 ; i < numRings; i++){
+        stroke(color(i*TWO_PI/numRings,0.9,0.9))
         push()
         translate(width * 0.5, height*0.5)
-        arc(0, 0,  initRadius+i*strWeight*2,initRadius+ i*strWeight*2, HALF_PI+QUARTER_PI, TWO_PI+QUARTER_PI)
+        rotate( sin(millis()*0.001*(i*0.25+5)))
+        arc(0, 0, radius*2+strWeight*i*2,radius*2+strWeight*i*5, HALF_PI+QUARTER_PI, TWO_PI+QUARTER_PI)
         pop()
     }
 }
--- week 05/sketch.js
+++ week 05/sketch.js
@@ -1,7 +1,39 @@
+let angle = 0.0
+let sizes = []
+let colors = []
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  colorMode(HSB, TWO_PI,1,1)
+  sizes[0]=200
+  sizes[1]=150
+  sizes[2]=165
+  sizes [3]=100
+  sizes[4]=80
+  sizes[5]=50
+
+  for (let i =5; i>=0; i--){
+    colors[i] = color(TWO_PI/6*i,0.5,0.8)
+  }
+  rectMode(RADIUS)
+  noStroke()
 }
 
 function draw() {
-  background(220);
+  background(0.2)
+
+  push()
+  translate(width/2,height/2)
+
+  for(let i = 0; i<sizes.length; i++){
+    fill(colors[i])
+    rotate(angle)
+    rect(0,0,sizes [3], sizes[i])
+    angle = angle + 0.003
+
+  }
+  pop()
+  
+
+
 }
