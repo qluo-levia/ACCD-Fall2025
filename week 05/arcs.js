538aeb473a1da4e56beb0487e484c75a51102188
qluo-levia <qluo@inside.artcenter.edu>
2025-10-30 19:34:11 -0700

    .

--- week 05/arcs.js
+++ week 05/arcs.js
@@ -9,16 +9,18 @@ function setup(){
     strokeCap(circle)
 }
 
+
 function draw(){
+    
     background(0)
     noFill()
-    strokeWeight(strWeight)
+    strokeWeight(strWeight*0.5)
     for (let i = 0 ; i < numRings; i++){
         stroke(color(i*TWO_PI/numRings,0.9,0.9))
         push()
         translate(width * 0.5, height*0.5)
         rotate( sin(millis()*0.001*(i*0.25+5)))
-        arc(0, 0, radius*2+strWeight*i*2,radius*2+strWeight*i*5, HALF_PI+QUARTER_PI, TWO_PI+QUARTER_PI)
+        arc(0, 0, radius*2+strWeight*i*2,radius*2+strWeight*i*2, HALF_PI+QUARTER_PI, TWO_PI+QUARTER_PI)
         pop()
     }
 }
