d12782d72865dd3e75d4aae0a7a3a67c5d9a521a
qluo-levia <qluo@inside.artcenter.edu>
2025-12-19 10:08:27 -0800

    .

--- week 04/homework/sketch.js
+++ week 04/homework/sketch.js
@@ -1,27 +1,27 @@
 let posX, posY, velX, velY
 let diameter = 150
 
-let imgPingu,imgTowel,currentImg
+let imgPingu, imgTowel, imgBackground, currentImg
 let countDown = 0
+
+let noiseOffsetX = 0
+let noiseOffsetY = 1000
 
 function preload(){
   imgTowel = loadImage("AngryTowelPingu.jpg")
   imgPingu = loadImage("AngryPingu.jpg")
-   imgBackground = loadImage("Background.jpg")
+  imgBackground = loadImage("Background.jpg")
 }
 
 function setup() {
   createCanvas(800, 600)
-  background(imgBackground, diameter)
   imageMode(CENTER)
-  
-  
-  posX = width/2
-  posY = height/2
-  
-  velX = random(-5,5)
-  velY = random(-3.5,3.5)
-  
+
+  posX = width / 2
+  posY = height / 2
+
+  velX = random(-3, 3)
+  velY = random(-3, 3)
 
   currentImg = imgPingu
 }
@@ -29,34 +29,39 @@ function setup() {
 function draw() {
   image(imgBackground, width/2, height/2, width, height)
 
-  
+  // ---------- 非线性速度变化 ----------
+  let accelX = map(noise(noiseOffsetX), 0, 1, -0.3, 0.3)
+  let accelY = map(noise(noiseOffsetY), 0, 1, -0.3, 0.3)
+
+  velX += accelX
+  velY += accelY
+
+  noiseOffsetX += 0.01
+  noiseOffsetY += 0.01
+  // -----------------------------------
+
+  posX += velX
+  posY += velY
+
   image(currentImg, posX, posY, diameter, diameter)
-  
-  posX = posX+velX
-  posY = posY+velY
-  
-  if(posX + diameter*0.5 >= width || posX - diameter*0.5 <= 0){
-    velX = velX*-1;
+
+  // ---------- bounce ----------
+  if (posX + diameter/2 >= width || posX - diameter/2 <= 0) {
+    velX *= -1
     currentImg = imgTowel
     countDown = 16
   }
-  
-  
-  if(posY + diameter*0.5 >=height || posY - diameter*0.5 <=0){
-    velY = velY * -1
+
+  if (posY + diameter/2 >= height || posY - diameter/2 <= 0) {
+    velY *= -1
     currentImg = imgTowel
     countDown = 16
   }
-    
-    if(countDown > 0){
-      countDown--
-    }
-    else{
-      currentImg = imgPingu
-    }
+  // ---------------------------
 
-
-  posX = posX+velX
-  posY = posY+velY
-
+  if (countDown > 0) {
+    countDown--
+  } else {
+    currentImg = imgPingu
   }
+}
